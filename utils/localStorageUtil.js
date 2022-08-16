8390fef640564f78ef5e53455515e0ace7c4f43e
VladimirBerzin <102286159+VladimirBerzin@users.noreply.github.com>
2022-08-16 18:09:01 +0300

    Add files via upload

---
+++ utils/localStorageUtil.js
@@ -0,0 +1,31 @@
+class LocalStorageUtil {
+  constructor() {
+    this.keyName = 'products';
+  }
+
+  getProducts() {
+    const productsLocalStorage = localStorage.getItem(this.keyName);
+    if (productsLocalStorage !== null) {
+      return JSON.parse(productsLocalStorage);
+    }
+    return [];
+  }
+  putProducts(id) {
+    let products = this.getProducts();
+    const index = products.indexOf(id);
+    let pushProduct = false;
+    if (index === -1) {
+      products.push(id);
+      pushProduct = true;
+    } else {
+      products.splice(index, 1);
+    }
+    localStorage.setItem(this.keyName, JSON.stringify(products));
+    return {
+      pushProduct,
+      products,
+    };
+  }
+}
+
+const localStoregeUtil = new LocalStorageUtil();
